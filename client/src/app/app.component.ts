3a8cd83324fd46fed42d4644196e2d44c46f1142
JBurnes <jesusburnesvela@gmail.com>
2020-11-09 12:56:13 -0600

    3.24 Adding CORS support in the API

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent  implements OnInit{
   }
 
   getUsers(){
-    this.http.get('http://localhost:5001/api/users').subscribe(response => {
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
       this.users = response; 
     },error => {
       console.log(error);
