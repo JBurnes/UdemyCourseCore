f32aa80198b5481b5e139830608d2d1fb865fee7
JBurnes <jesusburnesvela@gmail.com>
2020-11-12 12:22:42 -0600

    4.43 Creating a Uer DTO and returning the token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -15,21 +15,23 @@ using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
+using API.Interfaces;
 namespace API.Controllers
 {
     
     public class AccountController:BaseApiController
     {
         private readonly DataContext _context;
-        public  AccountController(DataContext context)
+        private readonly ITokenService _tokenService;
+        public  AccountController(DataContext context, ITokenService tokenService)
         {
+            _tokenService = tokenService;
             _context = context;
         }
 
         [HttpPost("register")]
         //public async Task<ActionResult<AppUser>> Register(string username, string password)
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
@@ -45,7 +47,10 @@ namespace API.Controllers
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return user;
+            return new UserDto{
+                Username= user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         private async Task<bool> UserExists(string username)
@@ -53,17 +58,21 @@ namespace API.Controllers
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
-            if(user == null )return Unauthorized("Invalid User");
+            if(user == null )return Unauthorized("Invalid Username");
             using var hmac =new HMACSHA512(user.PasswordSalt);
             var computedHash= hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
             for(int i  = 0  ; i < computedHash.Length; i++)
             {
                 if(computedHash[i] != user.PasswordHash [i]) return Unauthorized("Invalid password");
             }
-            return user;
+
+             return new UserDto{
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
     }
 }
