3f542ce8df98238c32667d0d3332489e5f3441cb
JBurnes <jesusburnesvela@gmail.com>
2020-11-13 16:41:54 -0600

    5.56 Using the asunc pipe

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,14 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../_modules/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+
+  
 baseUrl = "https://localhost:5001/api/"
-  constructor(private http: HttpClient) { }
+private currentUserSource = new ReplaySubject<User>(1);
+cuerrentUser$ =this.currentUserSource.asObservable();
+
+
+constructor(private http: HttpClient) { }
+
 login(model:any){
   //services singlenton this dont destroy when the app is dawn.
-  return this.http.post(this.baseUrl + 'account/login',model);
+  return this.http.post(this.baseUrl + 'account/login',model).pipe(
+    map((response:User) => {
+      const user =response;
+      if(user){
+        localStorage.setItem('user',JSON.stringify(user));
+        this.currentUserSource.next(user);
+
+      }
+    })
+  );
+}
+serCurrentUser(user:User){
+  this.currentUserSource.next(user);
+}
+logout(){
+  localStorage.removeItem('user');
+  this.currentUserSource.next(null);
 }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_modules/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -10,22 +13,36 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   
   model:any = {}
-  loggedIn:boolean;
-  constructor(private accountServices: AccountService) { }
+  users:any;
+   
+  constructor(private http: HttpClient, public accountServices: AccountService) { }
 
   ngOnInit(): void {
-
+   this.getUsers();
+   this.setCurrentUser();
   }
-  
-  login(){
+
+
+  setCurrentUser()
+{
+  const user: User = JSON.parse(localStorage.getItem('user'));
+  this.accountServices.serCurrentUser(user);
+} 
+getUsers()
+{
+  this.http.get('https://localhost:5001/api/users').subscribe(response =>{
+    this.users =response;
+  })
+}
+ login(){
     this.accountServices.login(this.model).subscribe(response =>{
       console.log(response);
-      this.loggedIn = true;
     },error => {
       console.log(error);
     })    
   }
   logout(){
-    this.loggedIn = false;
+    this.accountServices.logout();
   }
+  
 }
