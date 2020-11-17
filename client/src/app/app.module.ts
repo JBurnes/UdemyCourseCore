73bc22f47de947432be5b8fc1942f8f3ffc11921
JBurnes <jesusburnesvela@gmail.com>
2020-11-17 00:36:29 -0600

    5. 58 Adding a register form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,12 +10,14 @@ import { FormsModule } from '@angular/forms';
 import { isPlatformBrowser } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+  
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+
+}
