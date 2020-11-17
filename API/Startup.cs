14ba8b66eb164341ccaf145bc1a4de13d0ad0967
JBurnes <jesusburnesvela@gmail.com>
2020-11-17 15:25:50 -0600

    7.76. Exception handling middleware

--- API/Startup.cs
+++ API/Startup.cs
@@ -20,6 +20,8 @@ using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using API.Extensions;
+using API.Middleware;
+
 namespace API
 {
     public class Startup
@@ -54,13 +56,14 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            /*
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
-
+            }*/
+            app.UseMiddleware<ExceptionMiddleware>();
             app.UseHttpsRedirection();
 
             app.UseRouting();
