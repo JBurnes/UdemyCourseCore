31ec3e0414ca44d9f4556fffe350ae2adf9823a3
JBurnes <jesusburnesvela@gmail.com>
2020-11-27 12:36:12 -0600

    fix 9. 109

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,26 +1,22 @@
-
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using Microsoft.AspNetCore.Authorization;
 using API.Interfaces;
 using AutoMapper;
-using API.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    //TODO fix token
-   // [Authorize]  comment becoause token do not work.
-   
+    [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly IMapper _mapper;
-
         private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
         public UsersController(IUserRepository userRepository, IMapper mapper)
         {
             _mapper = mapper;
@@ -28,14 +24,14 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-           var users = await _userRepository.GetMembersAsync();
+            var users = await _userRepository.GetMembersAsync();
 
             return Ok(users);
         }
 
-        [HttpGet("username")]
+        [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
