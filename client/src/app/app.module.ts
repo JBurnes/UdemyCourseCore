a18706e0668afef6cb57f995ee11722ae91dd743
JBurnes <jesusburnesvela@gmail.com>
2020-11-27 11:27:32 -0600

    9. 107. Using an interceptor to send the token

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -48,6 +49,7 @@ import { MemberCardComponent } from './members/member-card/member-card.component
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
