3564e326d72b103e9c94e8d8f081ed986463810f
JBurnes <jesusburnesvela@gmail.com>
2020-11-30 17:52:00 -0600

    10. 21. Using the service to store state  error 0.1

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { unescapeIdentifier } from '@angular/compiler';
 import { Injectable } from '@angular/core';
+import { of, zip } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
@@ -20,16 +23,35 @@ const httpOptions = {
 export class MembersService 
 {
   baseUrl= environment.apiUrl;
+  memebers:Member[] =[];
   constructor(private http: HttpClient) {}
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if (this.memebers.length >0)return of (this.memebers);
+
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members =>{
+        this.memebers = members;
+        return members;
+      })
+    )   
   }
 
   getMember(username: string) {
+    const member = this.memebers.find(x => x.username === username);
+    if (member !== undefined)return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
-  updateMember(member: Member){
-    return this.http.put(this.baseUrl + 'users',member);
+
+  updateMember(member: Member)
+  {
+    return this.http.put(this.baseUrl + 'users',member).pipe(
+
+      map(()=>{
+      const index =this.memebers.indexOf(member);
+      this.memebers[index] = member;
+    })
+    )
+    
   }
   
 }
