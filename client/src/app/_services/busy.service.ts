41a1a73b4829c94f138a91b526d4cca2030d27ce
JBurnes <jesusburnesvela@gmail.com>
2020-11-30 18:34:11 -0600

    10. 121. Using the service to store state  fix

--- client/src/app/_services/busy.service.ts
+++ client/src/app/_services/busy.service.ts
@@ -5,15 +5,17 @@ import { NgxSpinnerService } from 'ngx-spinner';
   providedIn: 'root'
 })
 export class BusyService {
-  busyRequestCount: 0;
+  busyRequestCount = 0;
 
-  constructor(private spinnerService:NgxSpinnerService) { }
+
+  constructor(private spinnerService: NgxSpinnerService) { }
+
   busy(){
     this.busyRequestCount ++;
     this.spinnerService.show(undefined,{
       type:'line-scale-party',
       bdColor:'rgba(255,255,255,0)',
-      color:'#333333',
+      color:'#333333'
     }) 
   }
 
