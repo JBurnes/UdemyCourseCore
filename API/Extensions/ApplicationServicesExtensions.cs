458c01c4a59a09f9205d45b5163eda429dcb7152
JBurnes <jesusburnesvela@gmail.com>
2020-12-01 10:02:41 -0600

    11.126. Adding a photo service

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -16,8 +16,9 @@ namespace API.Extensions
     {
         public static IServiceCollection  AddApplicationServices(this IServiceCollection services , IConfiguration config)
         {
-                services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+                services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));                
                 services.AddScoped<ITokenService,TokenService>();
+                services.AddScoped<IPhotoService,PhotoServices>();
                 services.AddScoped<IUserRepository,UserRepository>();
                 services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly); 
                 services.AddDbContext<DataContext>(options =>{
