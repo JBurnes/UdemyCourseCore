8a6beedbe7f1e5d3766369f2aa624a921b2a7336
JBurnes <jesusburnesvela@gmail.com>
2020-12-02 12:46:36 -0600

    12.142. Adding custom validators

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractConstructor } from '@angular/material/core/common-behaviors/constructor';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -25,11 +26,16 @@ initializeForm(){
   this.registerForm = new FormGroup({
     username: new FormControl('Hello',Validators.required),
     password: new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(8)]),
-    confirmPassword:new FormControl('',Validators.required)
+  confirmPassword:new FormControl('',[Validators.required, this.matchValues('password')])
   })
   
 }
  
+matchValues(matchTo: string):ValidatorFn{
+  return (control: AbstractControl)=>{
+    return control?.value === control?.parent?.controls[matchTo].value ? null: {isMatching: true}
+  }
+}
   register() {
     console.log(this.registerForm.value);
     // this.accountService.register(this.model).subscribe(response => {
