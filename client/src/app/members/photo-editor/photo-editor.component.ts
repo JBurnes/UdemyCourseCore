915d6334ec44015db8cc914c761fdbabdc6ad32a
JBurnes <jesusburnesvela@gmail.com>
2020-12-02 11:52:35 -0600

    11.137. Deleting photos - Client

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -47,7 +47,11 @@ constructor(private accountService: AccountService, private memberService: Membe
       })
     })
   } 
-
+  deletePhoto(photoId: number) {
+    this.memberService.deletePhoto(photoId).subscribe(() => {
+      this.member.photos = this.member.photos.filter(x => x.id !== photoId);
+    })
+  }
 
 initializeUploader(){
   this.uploader = new FileUploader({
