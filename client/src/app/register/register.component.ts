486260fb83ccd0fdfb0c743ab642909a84bb7f5f
JBurnes <jesusburnesvela@gmail.com>
2020-12-02 12:34:42 -0600

    11.140. Reactive forms introduction

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -11,21 +12,33 @@ export class RegisterComponent implements OnInit {
   @Input() usersFromComponent: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm:FormGroup;
+
 
   constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+this.initializeForm();
   }
-
+  
+initializeForm(){
+  this.registerForm = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+    confirmPassword:new FormControl()
+  })
+  
+}
  
   register() {
-    this.accountService.register(this.model).subscribe(response => {
-      console.log(response);
-      this.cancel();
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
-    })
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.model).subscribe(response => {
+    //   console.log(response);
+    //   this.cancel();
+    // }, error => {
+    //   console.log(error);
+    //   this.toastr.error(error.error);
+    // })
   }
 
   cancel() {
