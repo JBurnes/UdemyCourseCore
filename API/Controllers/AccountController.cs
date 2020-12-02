9f178c469a35e09094456751c73a9c477833f887
JBurnes <jesusburnesvela@gmail.com>
2020-12-02 10:18:24 -0600

    11.134. Adding the main photo image to the nav bar

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System;
 using System.Reflection.Metadata;
 using System.Net.Mime;
@@ -49,7 +50,8 @@ namespace API.Controllers
 
             return new UserDto{
                 Username= user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl= user.Photos.FirstOrDefault(x => x.IsMain).Url
             };
         }
 
@@ -62,7 +64,10 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            
+            var user = await _context.Users
+            .Include(p=>p.Photos)
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
             if(user == null )return Unauthorized("Invalid Username"); //if type a invalid username
             using var hmac =new HMACSHA512(user.PasswordSalt);
@@ -75,7 +80,8 @@ namespace API.Controllers
 
              return new UserDto{
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl= user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
     }
