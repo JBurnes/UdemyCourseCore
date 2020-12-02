53d60f198aeb2dae1ef83afcde5fc20d4d1c7b43
JBurnes <jesusburnesvela@gmail.com>
2020-12-02 12:34:42 -0600

    12.140. Reactive forms introduction

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,15 +6,12 @@ import { AppComponent } from './app.component';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
-import { isPlatformBrowser } from '@angular/common';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
-import { Toast, ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
@@ -55,6 +52,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
    HttpClientModule,
    BrowserAnimationsModule,
    FormsModule,
+   ReactiveFormsModule,
    SharedModule,
    MatInputModule,
    MatTabsModule,
