98a559baab994597499a57b5c56ffa65c12dc86d
JBurnes <jesusburnesvela@gmail.com>
2020-12-10 17:38:00 -0600

    15.183. Setting up the automapper profiles

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -1,7 +1,9 @@
+using System.Runtime.CompilerServices;
 using System.Linq;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Interfaces;
 using AutoMapper;
 
 namespace API.Helpers
@@ -18,6 +20,13 @@ namespace API.Helpers
             CreateMap<MemberUpdateDto,AppUser>();
             CreateMap<RegisterDto,AppUser>();
 
+            //IMappingExpression<TSource, TDestination> 
+            //ForMember<TMember>(Expression<Func<TDestination, TMember>> destinationMember, Action<IMemberConfigurationExpression<TSource, TDestination, TMember>> memberOptions);
+            CreateMap<Message, MessageDto>()
+            .ForMember(dest => dest.SenderPhotoUrl, opt=> opt.MapFrom(src => src.Sender.Photos.FirstOrDefault(x =>x.IsMain).Url));
+
+             CreateMap<Message, MessageDto>()
+            .ForMember(dest => dest.RecipientPhotoUrl, opt=> opt.MapFrom(src => src.Sender.Photos.FirstOrDefault(x =>x.IsMain).Url));
         }
     }
 }
