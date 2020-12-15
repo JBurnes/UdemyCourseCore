900606f67b2a2bf87a0db833040e82d67b7bfa6d
JBurnes <jesusburnesvela@gmail.com>
2020-12-15 17:57:12 -0600

    16.205. Updating the account controller

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,39 +1,31 @@
-using System.Globalization;
 using System.Linq;
-using System;
-using System.Reflection.Metadata;
-using System.Net.Mime;
-using System.Diagnostics.Tracing;
-using System.Xml.XPath;
-using System.Xml.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Xml.Schema;
-using System.Data.Common;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using API.Interfaces;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
 
     public class AccountController : BaseApiController
     {
-        private readonly DataContext _context;
-        private readonly ITokenService _tokenService;
+       private readonly ITokenService _tokenService;
         private readonly IMapper _mapper;
-        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService, IMapper mapper)
         {
+            _signInManager = signInManager;
+            _userManager = userManager;
             _mapper = mapper;
             _tokenService = tokenService;
-            _context = context;
-
         }
 
         [HttpPost("register")]
@@ -41,15 +33,17 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
-                var user = _mapper.Map<AppUser>(registerDto);
-           
-           
-               user.UserName = registerDto.Username.ToLower();
-               
-            
 
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+            var user = _mapper.Map<AppUser>(registerDto);
+
+            user.UserName = registerDto.Username.ToLower();
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+                 
+                       
 
             return new UserDto
             {
@@ -63,7 +57,7 @@ namespace API.Controllers
 
         private async Task<bool> UserExists(string username)
         {
-            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+            return await _userManager.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
 
 
@@ -71,13 +65,15 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
 
-            var user = await _context.Users
+            var user = await _userManager.Users
             .Include(p => p.Photos)
-            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
             if (user == null) return Unauthorized("Invalid Username"); //if type a invalid username
          
-            
+             var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+              if (!result.Succeeded) return Unauthorized();
 
             return new UserDto
             {
