712dce184b7cc4230e724f9616ccd586938a5b10
JBurnes <jesusburnesvela@gmail.com>
2020-12-16 10:39:52 -0600

    16. 208. Adding policy based authorisation

---
+++ API/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AdminController : BaseApiController
+    {
+       
+
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpGet("users-with-roles")]
+        public  ActionResult GetUsersWithRoles()
+        {           
+            return Ok("only admins can see this");
+        }
+
+      
+
+        [Authorize(Policy = "ModeratePhotoRole")]
+        [HttpGet("photos-to-moderate")]
+        public ActionResult GetPhotosForModeration()
+        {
+            return Ok("Admins or moderators can see this");
+        }
+    }
+}
