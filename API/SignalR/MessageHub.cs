d2176bd4e234da9972ba3bd67f352378877c1180
JBurnes <jesusburnesvela@gmail.com>
2020-12-18 15:11:56 -0600

    17. 226. Adding the send message method to the hub

--- API/SignalR/MessageHub.cs
+++ API/SignalR/MessageHub.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
 using API.Extensions;
 using API.Interfaces;
 using AutoMapper;
@@ -11,10 +13,14 @@ namespace API.SignalR
     {
         private readonly IMessageRepository _messageRepository;
         private readonly IMapper _mapper;
-        public MessageHub(IMessageRepository messageRepository, IMapper mapper)
+
+        private readonly IUserRepository _userRepository;
+        public MessageHub(IMessageRepository messageRepository, IMapper mapper,
+        IUserRepository userRepository)
         {
             _mapper = mapper;
             _messageRepository = messageRepository;
+            _userRepository = userRepository;
 
         }
 
@@ -22,22 +28,60 @@ namespace API.SignalR
         {
             var httpContext = Context.GetHttpContext();
             var otherUser = httpContext.Request.Query["user"].ToString();
-            var groupName = GetGroupName (Context.User.GetUsername(), otherUser);
+            var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
 
             var messages = await _messageRepository.
                             GetMessageThread(Context.User.GetUsername(), otherUser);
-            await Clients.Group(groupName).SendAsync("ReciveMessageThread",messages);
+            await Clients.Group(groupName).SendAsync("ReciveMessageThread", messages);
         }
 
-        public override async Task OnDisconnectedAsync( Exception exception)
+
+
+        public override async Task OnDisconnectedAsync(Exception exception)
         {
             await base.OnDisconnectedAsync(exception);
         }
-        private string GetGroupName (string caller , string other)
+        private string GetGroupName(string caller, string other)
         {
-            var stringCompare = string.CompareOrdinal(caller, other)<0;
-            return stringCompare ? $"{caller}-{other}": $"{other}-{caller}";
+            var stringCompare = string.CompareOrdinal(caller, other) < 0;
+            return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+        }
+
+        public async Task SendMessage(CreateMessageDto createMessageDto)
+        {
+            var username = Context.User.GetUsername();
+
+            if (username == createMessageDto.RecipientUsername.ToLower())
+                throw new HubException("You cannot send messages to yourself");
+
+            var sender = await _userRepository.GetUserByUsernameAsync(username);
+            var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+            if (recipient == null) 
+            {
+                throw new HubException ("Not found user");
+            }
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.UserName,
+                RecipientUsername = recipient.UserName,
+                Content = createMessageDto.Content
+            };
+
+            _messageRepository.AddMessage(message);
+
+            if (await _messageRepository.SaveAllAsync())
+            {
+                var group = GetGroupName (sender.UserName, recipient.UserName); 
+
+                await Clients.Group(group).SendAsync ("NewMessage", _mapper.Map<MessageDto>(message));
+            } 
+
+            
         }
     }
 }
