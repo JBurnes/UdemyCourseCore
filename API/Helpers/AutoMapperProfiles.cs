e7b758f23955a6b054d2b2b15357753939da783a
JBurnes <jesusburnesvela@gmail.com>
2020-12-21 15:50:23 -0600

    17.232. Dealing with UTC date formats

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Runtime.CompilerServices;
 using System.Linq;
 using API.DTOs;
@@ -27,6 +28,8 @@ namespace API.Helpers
 
              CreateMap<Message, MessageDto>()
             .ForMember(dest => dest.RecipientPhotoUrl, opt=> opt.MapFrom(src => src.Sender.Photos.FirstOrDefault(x =>x.IsMain).Url));
+
+            CreateMap<DateTime, DateTime>().ConvertUsing(d=>DateTime.SpecifyKind(d, DateTimeKind.Utc));
         }
     }
 }
